3deede310557c86b5128786e61fbbb7f134e7023
Keith Batten <kbatten@gmail.com>
2020-08-26 20:26:11 -0700

    add tool to convert json to a one liner

---
+++ bin/unjson_tool.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import sys
+import json
+
+def main():
+    if len(sys.argv) == 1:
+        infile = sys.stdin
+        outfile = sys.stdout
+    elif len(sys.argv) == 2:
+        infile = open(sys.argv[1], 'rb')
+        outfile = sys.stdout
+    elif len(sys.argv) == 3:
+        infile = open(sys.argv[1], 'rb')
+        outfile = open(sys.argv[2], 'wb')
+    else:
+        raise SystemExit(sys.argv[0] + "[infile [outfile]]")
+    with infile:
+        try:
+            obj = json.load(infile)
+        except ValueError, e:
+            raise SystemExit(e)
+    with outfile:
+        json.dump(obj, outfile, sort_keys=True, separators=(',', ': '))
+        outfile.write('\n')
+
+if __name__ == '__main__':
+    main()
+
